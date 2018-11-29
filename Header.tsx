86f6e762ce1806ef67d141a9598c30cd435ef3e7
Sandra Anil <sandraanil44@gmail.com>
2018-11-29 09:23:38 +0530

    Create Header.tsx

---
+++ Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Hero extends React.Component{
+    
+    render(){
+        return (
+                    <h1>Author Details</h1>
+                );
+        }
+}
+
+export default Hero;
