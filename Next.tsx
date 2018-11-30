e4f53e566d5c931d97f733be3f5d88ce29b1397e
Sandra Anil <sandraanil44@gmail.com>
2018-11-30 17:53:06 +0530

    Update Next.tsx

--- Next.tsx
+++ Next.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import reload_obj from '../../services/reload';
 
+interface INext {
+    click() : void;
+}
 
-export default class Next extends React.Component{
+export default class Next extends React.Component<INext,{}>{
 
-    
     render(){
         return(
-            <button className="btn btn-primary btn-lg button" onClick={reload_obj.nextQue}>Next</button>
+            <button className="btn btn-primary btn-lg button" onClick={this.props.click}>Next</button>
         )
     }
 }
