8b0157a42da2eac76fed8457d29706fe5ef1a8f5
Sandra Anil <sandraanil44@gmail.com>
2018-11-30 17:54:26 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Route, Router} from 'react-router-dom';
 import AuthorMain from './components/AuthorQuiz/AuthorMain';
-import history from './services/history';
-import reload_obj from './services/reload';
-import session_obj from './services/storage';
 
 const authors = [
   {
@@ -35,39 +31,4 @@ const authors = [
   }
 ] 
 
-const searchAuthor = () => {
-  let auth = {}
-  authors.map((a:any) => {if(a.name === session_obj.getVal("search_name")){auth = a;}});
-  return auth;
-}
-
-const app = () => {
-  reload_obj.firstTime();
-  return <AuthorMain author={search()} />;
-}
-
-const search = () => {
-  let author = {};
-  author = session_obj.getVal("search") ? searchAuthor() : getAuthor();
-  console.log(author);
-  session_obj.removeVal("search");
-  return author;
-}
-
-const getAuthor = () => {
-  if(!session_obj.getVal("index")){
-    session_obj.setVal("index",'0');
-  }
-  else if(parseInt(session_obj.getVal("index"),10) > 3){
-    session_obj.setVal("index",'0');
-  }
-  const ind = parseInt(session_obj.getVal("index"),10);
- // console.log(session_obj.getVal("index"));
-  return authors[ind];
-}
-
-ReactDOM.render(<Router history={history}>
-  <React.Fragment>
-      <Route exact={true} path="/" component={app} />
-      </React.Fragment>
-</Router>,document.getElementById('root') as HTMLElement);
+ReactDOM.render(<AuthorMain authors={authors} />,document.getElementById('root') as HTMLElement);
